d2d9dbd6788f32f6e27af7421eb6f237c9c4c1de
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-04-22 15:36:23 +0200

    Parsing input and storing as building description and floor requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::io::{self, Read};
+use std::fs::File;
+
 pub fn run_simulation(){
     let mut location: f64 = 0.0; // meters 
     let mut velocity: f64 = 0.0; // meters per second 
@@ -9,6 +13,32 @@ pub fn run_simulation(){
     let mut floor_count: u64 = 0;
     let mut floor_height: f64 = 0.0;
     let mut floor_request: Vec<u64> = Vec::new();
+
+    let buffer = match env::args().nth(1) {
+        Some(ref fp) if *fp == "-".to_string() => {
+            let mut buffer = String::new();
+            io::stdin().read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        },
+        None => {
+            let fp = "test1.txt";
+            let mut buffer = String::new();
+            File::open(fp)
+                .expect("File::open failed")
+                .read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        },
+        Some(fp) => {
+            let mut buffer = String::new();
+            File::open(fp)
+                .expect("File::open failed")
+                .read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        }
+    };
 
     while floor_request.len() > 0 {
         // Update location, velocity, and acceleration
