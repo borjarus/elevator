b98e66dcdc72a87404794ea94e1b5cd4c1cc8cc7
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-04-22 15:59:25 +0200

    Updating location, velocity, and acceleration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::env;
+use std::{env, time};
 use std::io::{self, Read};
 use std::fs::File;
+use std::time::Instant;
+use std::thread;
+
 
 pub fn run_simulation(){
     let mut location: f64 = 0.0; // meters 
@@ -40,8 +43,38 @@ pub fn run_simulation(){
         }
     };
 
+    for (li,l) in buffer.lines().enumerate() {
+        if li == 0 {
+            floor_count = l.parse().unwrap();
+        } else if li == 1 {
+            floor_height = l.parse().unwrap();
+        } else {
+            floor_request.push(l.parse().unwrap());
+        }
+    }
+
+    
+    
+    
     while floor_request.len() > 0 {
         // Update location, velocity, and acceleration
+        let mut prev_loop_time = Instant::now();
+    
+        let now = Instant::now();
+        let dt = now.duration_since(prev_loop_time)
+                        .as_secs_f64();
+        prev_loop_time = now;
+    
+        thread::sleep(time::Duration::from_millis(10)); 
+
+        location = location + velocity * dt;
+        velocity = velocity + acceleration * dt;
+        acceleration = {
+            let F = (up_input_voltage - down_input_voltage) * 8.0;
+            let m = 1200000.0;
+            -9.8 + F/m
+        };
+        
 
         // If next floor request in queue is satisfied, then remove from queue
 
